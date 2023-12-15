57218a78725428f4c4d5816ebaa4da1599cf4b71
Your Username <parhambarati32@gmail.com>
2023-12-14 20:07:28 -0500

    Header and Footer are good to go

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  root: true,
+  root: false,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+  },
 }
